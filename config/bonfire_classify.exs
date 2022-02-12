05e946d17b9991c4167e1f75410899d2162885bc
Mayel de Borniol <git@mayel.space>
2022-02-12 16:49:56 +1300

    clean

--- config/bonfire_classify.exs
+++ config/bonfire_classify.exs
@@ -6,6 +6,4 @@ config :bonfire_classify,
 config :bonfire_classify, Bonfire.Classify.Category,
   has_one: [actor:        {Bonfire.Data.ActivityPub.Actor,     foreign_key: :id}],
   has_one: [character:    {Bonfire.Data.Identity.Character,    foreign_key: :id}],
-  has_one: [follow_count: {Bonfire.Data.Social.FollowCount,    foreign_key: :id}],
-  has_one: [like_count:   {Bonfire.Data.Social.LikeCount,      foreign_key: :id}],
   has_one: [profile:      {Bonfire.Data.Social.Profile,        foreign_key: :id}]
