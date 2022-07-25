9b06a50134fd021fc5764c68688dc94db9babfcc
ivanminutillo <ivanminutillo@outlook.it>
2022-07-25 12:31:30 +0200

    https://github.com/bonfire-networks/bonfire-app/issues/424

--- lib/web/components/nav/categories_nav_live.ex
+++ lib/web/components/nav/categories_nav_live.ex
@@ -1,5 +1,21 @@
 defmodule Bonfire.Classify.Web.CategoriesNavLive do
-  use Bonfire.UI.Common.Web, :stateless_component
+  use Bonfire.UI.Common.Web, :stateful_component
 
+  def update(assigns, socket) do
+    params = e(assigns, :__context__, :current_params, %{})
+    topics = Bonfire.Social.Follows.list_my_followed(current_user(assigns), pagination: false, type: Bonfire.Classify.Category)  |> IO.inspect(label: "TESTTTT")
 
+    {:ok, socket
+      |> assign(assigns)
+      |> assign(
+        topics: e(topics, :edges, [])
+      )
+    }
+  end
+
+  def category_link(category) do
+    id = e(category, :character, :username, nil) || e(category, :id, "#no-parent")
+
+    "/+" <> id
+  end
 end
