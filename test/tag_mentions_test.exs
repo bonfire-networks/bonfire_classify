679cba1c6c09c20ecca539d079c989065d649ed5
Mayel de Borniol <git@mayel.space>
2022-08-27 08:33:52 +1200

    tests

--- test/tag_mentions_test.exs
+++ test/tag_mentions_test.exs
@@ -17,40 +17,40 @@ defmodule Bonfire.Classify.TagMentionsTest do
     assert String.contains?(post.post_content.html_body, "+#{mentioned.character.username}")
   end
 
-  test "can see post mentioning a category in its notifications (using the 'mentions' preset), ignoring boundaries" do
-    me = Fake.fake_user!()
-    other = Fake.fake_user!()
-    mentioned = fake_category!(other)
-    attrs = %{post_content: %{html_body: "+#{mentioned.character.username} this is very on topic</p>"}}
-    assert {:ok, mention} = Posts.publish(current_user: me, post_attrs: attrs, boundary: "mentions")
-    assert %{edges: feed} = FeedActivities.feed(:notifications, current_user: mentioned, skip_boundary_check: true)
-    # debug(feed)
-    assert %{} = fp = List.first(feed)
-    assert fp.activity.object_id == mention.id
-  end
+  # test "can see post mentioning a category in its notifications (using the 'mentions' preset), ignoring boundaries" do
+  #   me = Fake.fake_user!()
+  #   other = Fake.fake_user!()
+  #   mentioned = fake_category!(other)
+  #   attrs = %{post_content: %{html_body: "+#{mentioned.character.username} this is very on topic</p>"}}
+  #   assert {:ok, mention} = Posts.publish(current_user: me, post_attrs: attrs, boundary: "mentions")
+  #   assert %{edges: feed} = FeedActivities.feed(:notifications, current_user: mentioned, skip_boundary_check: true)
+  #   # debug(feed)
+  #   assert %{} = fp = List.first(feed)
+  #   assert fp.activity.object_id == mention.id
+  # end
 
-  test "can see post mentioning a category in its notifications feed (using the 'mentions' preset), with boundaries enforced" do
-    me = Fake.fake_user!()
-    other = Fake.fake_user!()
-    mentioned = fake_category!(other)
-    attrs = %{post_content: %{html_body: "+#{mentioned.character.username} this is very on topic</p>"}}
-    assert {:ok, mention} = Posts.publish(current_user: me, post_attrs: attrs, boundary: "mentions")
-    assert %{edges: feed} = FeedActivities.feed(:notifications, current_user: mentioned)
-    assert %{} = fp = List.first(feed)
-    assert fp.activity.object_id == mention.id
-  end
+  # test "can see post mentioning a category in its notifications feed (using the 'mentions' preset), with boundaries enforced" do
+  #   me = Fake.fake_user!()
+  #   other = Fake.fake_user!()
+  #   mentioned = fake_category!(other)
+  #   attrs = %{post_content: %{html_body: "+#{mentioned.character.username} this is very on topic</p>"}}
+  #   assert {:ok, mention} = Posts.publish(current_user: me, post_attrs: attrs, boundary: "mentions")
+  #   assert %{edges: feed} = FeedActivities.feed(:notifications, current_user: mentioned)
+  #   assert %{} = fp = List.first(feed)
+  #   assert fp.activity.object_id == mention.id
+  # end
 
-  test "mentioning a category (which I don't have tag permission on) appears in its notifications feed, if using the 'mentions' preset" do
-    me = Fake.fake_user!()
-    other = Fake.fake_user!()
-    mentioned = fake_category!(other)
-    attrs = %{post_content: %{html_body: "+#{mentioned.character.username} this is very on topic</p>"}}
-    assert {:ok, mention} = Posts.publish(current_user: me, post_attrs: attrs, boundary: "mentions")
-    debug_my_grants_on(mentioned, mention)
-    assert %{edges: feed} = FeedActivities.feed(:notifications, current_user: mentioned)
-    assert %{} = fp = List.first(feed)
-    assert fp.activity.object_id == mention.id
-  end
+  # test "mentioning a category (which I don't have tag permission on) appears in its notifications feed, if using the 'mentions' preset" do
+  #   me = Fake.fake_user!()
+  #   other = Fake.fake_user!()
+  #   mentioned = fake_category!(other)
+  #   attrs = %{post_content: %{html_body: "+#{mentioned.character.username} this is very on topic</p>"}}
+  #   assert {:ok, mention} = Posts.publish(current_user: me, post_attrs: attrs, boundary: "mentions")
+  #   debug_my_grants_on(mentioned, mention)
+  #   assert %{edges: feed} = FeedActivities.feed(:notifications, current_user: mentioned)
+  #   assert %{} = fp = List.first(feed)
+  #   assert fp.activity.object_id == mention.id
+  # end
 
   test "mentioning a category appears in its outbox feed, if using the 'mentions' preset" do
     me = Fake.fake_user!()
@@ -64,7 +64,7 @@ defmodule Bonfire.Classify.TagMentionsTest do
   end
 
 
-  test "mentioning a category does not appear in my own notifications" do
+  test "mentioning a category appears in my own notifications, if I have :create permission on it" do
     me = Fake.fake_user!()
     mentioned = fake_category!(me)
     attrs = %{post_content: %{html_body: "+#{mentioned.character.username} this is very on topic</p>"}}
@@ -72,13 +72,13 @@ defmodule Bonfire.Classify.TagMentionsTest do
     assert %{edges: []} = FeedActivities.feed(:notifications, current_user: me)
   end
 
-  test "mentioning a category else does not appear in a 3rd party's notifications" do
+  test "mentioning a category does NOT appear in my own notifications, if I don't have permission" do
     me = Fake.fake_user!()
-    mentioned = fake_category!(me)
+    other = Fake.fake_user!()
+    mentioned = fake_category!(other, nil, %{boundary: "mentions"})
     attrs = %{post_content: %{html_body: "+#{mentioned.character.username} this is very on topic</p>"}}
     assert {:ok, mention} = Posts.publish(current_user: me, post_attrs: attrs, boundary: "mentions")
-    third = Fake.fake_user!()
-    assert %{edges: []} = FeedActivities.feed(:notifications, current_user: third)
+    assert %{edges: []} = FeedActivities.feed(:notifications, current_user: me)
   end
 
   test "mentioning a category appears in my instance feed (if using 'local' preset)" do
