4062a1de11c4ad59181af684adb71327a4827dcb
Mayel de Borniol <git@mayel.space>
2022-09-29 20:34:03 +1300

    props

--- lib/web/components/widgets/widget_subtopics/widget_subtopics_live.ex
+++ lib/web/components/widgets/widget_subtopics/widget_subtopics_live.ex
@@ -1,7 +1,7 @@
 defmodule Bonfire.Classify.Web.WidgetSubtopicsLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
-  prop widget_title, :string
-  prop subcategories, :list
-  prop category, :list
+  prop widget_title, :string, default: nil
+  prop subcategories, :list, default: []
+  prop category, :list, default: []
 end
