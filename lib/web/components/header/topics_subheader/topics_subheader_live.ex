b91ca17625bf4ca50f23809f6a80e69bc1b3c17c
ivanminutillo <ivanminutillo@outlook.it>
2022-10-22 22:37:24 +0200

    subheadeer

---
+++ lib/web/components/header/topics_subheader/topics_subheader_live.ex
@@ -0,0 +1,28 @@
+defmodule Bonfire.Classify.Web.TopicsSubheaderLive do
+  use Bonfire.UI.Common.Web, :stateful_component
+
+  def update(assigns, socket) do
+    params = e(assigns, :__context__, :current_params, %{})
+    limit = Bonfire.Common.Config.get(:default_pagination_limit, 10)
+
+    {:ok, categories} =
+      Bonfire.Classify.GraphQL.CategoryResolver.categories_toplevel(
+        %{limit: limit},
+        %{context: %{current_user: current_user(socket)}}
+      )
+
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign(
+        categories: e(categories, :edges, []),
+        limit: limit
+     )}
+  end
+
+  def category_link(category) do
+    id = e(category, :character, :username, nil) || e(category, :id, "#no-parent")
+
+    "/+" <> id
+  end
+end
