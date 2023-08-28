ea8862e8901f31b6818de2a261a376c78cd277f7
Mayel de Borniol <git@mayel.space>
2023-08-28 15:40:14 +0100

    account

--- lib/classify.ex
+++ lib/classify.ex
@@ -45,25 +45,15 @@ defmodule Bonfire.Classify do
   end
 
   def ensure_update_allowed(user, c) do
-    debug(user)
+    # debug(user)
 
     not is_nil(user) and
       (Types.ulid(user) ==
          (Utils.e(c, :creator, :id, nil) ||
             Utils.e(c, :created, :creator_id, nil)) or
-         is_admin?(user) ||
          Bonfire.Boundaries.can?(user, :edit, c))
 
     # TODO: add admin permission too?
-  end
-
-  def is_admin?(user) do
-    if is_map(user) and Map.get(user, :instance_admin) do
-      Map.get(user.instance_admin, :is_instance_admin)
-    else
-      # FIXME
-      false
-    end
   end
 
   # def ensure_delete_allowed(user, c) do
